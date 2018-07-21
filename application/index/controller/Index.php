f7e612af1ac4e61793714cbcd51fba34e49276d6
skyline <1820120473@qq.com>
2018-07-22 00:05:11 +0800

    tp5

---
+++ application/index/controller/Index.php
@@ -0,0 +1,38 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        return json(array(
+            "meta" => array(
+                "serverName" => "IceCraft",
+                "implementationName" => "IceCraft Admin",
+                "implementationVersion" => "1.0.0"
+            ),
+            "skinDomains" => array(
+                ".blog.com"
+            )
+        ));
+    }
+
+    /**
+     * 加入服务器
+     */
+    public function join() {
+        $accessToken = input('accessToken','','trim');
+        $selectedProfile = input('selectedProfile','','trim');
+        $serverId = input('serverId','','trim');
+
+        if (empty($accessToken) || empty($serverId) || empty($selectedProfile)) {
+            return json(array(
+                'error' => "Requested range not satisfiable",
+                'errorMessage' => "参数错误",
+                'cause' => ''
+            ),416);
+        }
+
+
+    }
+}
