921ac6e955021b29bf5d63dec0b40f4c15458138
skyline <1820120473@qq.com>
2018-07-22 19:48:18 +0800

    授权

--- application/index/controller/Authserver.php
+++ application/index/controller/Authserver.php
@@ -19,14 +19,14 @@ Class Authserver extends Controller {
     public function authenticate() {
         iceLog("---用户登陆认证接口---");
         $data = input('param.');
-        $data['agent'] =   array (
-            'name' => 'Minecraft',
-            'version' => 1,
-        );
-        $data['password'] = '123456';
-        $data['clientToken'] = '83106648ff824745ac361f68ce36d13b';
-        $data['requestUser'] = true;
-        $data['username'] = 'demo@ice.com';
+//        $data['agent'] =   array (
+//            'name' => 'Minecraft',
+//            'version' => 1,
+//        );
+//        $data['password'] = '123456';
+//        $data['clientToken'] = '83106648ff824745ac361f68ce36d13b';
+//        $data['requestUser'] = true;
+//        $data['username'] = 'demo@ice.com';
 
         iceLog($data);
         if (empty($data['username']) || empty($data['password'])
