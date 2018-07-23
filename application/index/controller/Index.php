c2f91b4584203ed047b9a6d574acbc73a4a91ee6
skyline <1820120473@qq.com>
2018-07-23 14:52:58 +0800

    debug

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -17,12 +17,6 @@ Class Index extends Controller {
      * 添加认证服务器接口
      */
     public function index() {
-
-        $str = 'eyJ0aW1lc3RhbXAiOjE1MzIzMjM1MTU3MjEsInByb2ZpbGVJZCI6ImYyNTNiNWY5MDgxZTQ3N2ZhNWRhY2I4MjhlNTZkMTYwIiwicHJvZmlsZU5hbWUiOiJjaGFyYWN0ZXIxIiwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwczovL2V4YW1wbGUueWdnZHJhc2lsLnl1c2hpLm1vZS90ZXh0dXJlcy9kODAzYWQzZTYyYTE2NzU4M2U5MmZkNWNhNGNkNGU5MWU4MjkyY2FlMmUyYjA2YmI4NmJiMjk0YTc4MTM4MDhlIiwibWV0YWRhdGEiOnsibW9kZWwiOiJkZWZhdWx0In19LCJDQVBFIjp7InVybCI6Imh0dHBzOi8vZXhhbXBsZS55Z2dkcmFzaWwueXVzaGkubW9lL3RleHR1cmVzLzU5ZDlmNDMxZGQ5ZTVjOTVlMzhhMjcxODcwMjI1MWVkOGJkNGUyOWU0MTg2YjVkMmRlZWUzMTY3YTIzYjU0MmEifX19';
-        var_dump(json_decode(base64_decode($str)));
-        die;
-
-
         return json(array(
             "meta" => array(
                 "serverName" => "IceCraft",
